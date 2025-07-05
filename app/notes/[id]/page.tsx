834bc22635053167e63c52084e0f890c4656e7cd
Daria <darak0751@gmail.com>
2025-07-05 22:59:34 +0300

    hw7

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,12 +1,12 @@
 import NotePreview from '@/components/NotePreview/NotePreview';
+import type { JSX } from 'react';
 
 type PageProps = {
-  params: {
-    id: string;
-  };
+  params: { id: string };
 };
 
-export default async function NotePage({ params }: PageProps) {
+
+export default async function NotePage({ params }: PageProps): Promise<JSX.Element> {
   const id = Number(params.id);
 
   if (isNaN(id)) {
@@ -15,3 +15,4 @@ export default async function NotePage({ params }: PageProps) {
 
   return <NotePreview id={id} />;
 }
+
