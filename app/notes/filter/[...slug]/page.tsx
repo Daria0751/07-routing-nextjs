bc8c463872c0c9446065eb4dfc84da94a5d26c9c
Daria <darak0751@gmail.com>
2025-07-12 11:15:24 +0300

    hw7

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -3,17 +3,22 @@ import NotesClient from './Notes.client';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Notes page',
+  title: 'Filtered Notes',
 };
 
-export default async function NotesPage(
-  props: Promise<{ params: { slug: string[] } }>
-) {
-  const { params } = await props;
-  const tag = params.slug?.[0] || '';
+export default async function NotesFilteredPage({
+  params,
+}: {
+  params: { slug?: string[] };
+}) {
+  const slug = params.slug ?? [];
+  const tag = slug[0] || '';
   const safeTag = tag === 'All' ? '' : tag;
 
   const notes = await getNotes(safeTag, 1);
 
   return <NotesClient initialData={notes} tag={tag} />;
 }
+
+
+
